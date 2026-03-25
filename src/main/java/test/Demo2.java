4e3166c4fd933d1be52c1ace1bf5a968239e363d
Paresh_Patel2612 <paresh.green04@gmail.com>
2026-03-25 18:02:29 +0530

    Include/Exclude Mechanism & Package Level with Regex.

---
+++ src/main/java/test/Demo2.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+    @Test
+    public void pr(){
+        System.out.println("Demo 2 Class");
+    }
+
+    @Test
+    public void Car(){
+        System.out.println("Car");
+    }
+
+    @Test
+    public void Bike(){
+        System.out.println("Bike");
+    }
+}
